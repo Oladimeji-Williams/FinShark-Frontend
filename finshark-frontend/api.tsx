861d83bce7c44f5fe54e057ddc32f058362342ec
Oladimeji Williams <oladimejiwilliams@outlook.com>
2026-03-03 12:45:15 +0100

    17. useEffect - Added useEffect to load data into company page

--- finshark-frontend/api.tsx
+++ finshark-frontend/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { CompanySearch } from "@/company";
+import { CompanyProfile, CompanySearch } from "@/company";
 
 export const searchCompanies = async (request: string) => {
     try {
@@ -14,5 +14,35 @@ export const searchCompanies = async (request: string) => {
 
         console.error("Unexpected error searching for companies:", error);
         return "An unexpected error occurred while searching for companies.";
+    }
+}
+
+export const getCompanyProfile = async (ticker: string) => {
+    try {
+        const response = await axios.get<CompanyProfile[]>(`https://financialmodelingprep.com/stable/profile?symbol=${ticker}&apikey=${process.env.NEXT_PUBLIC_FMP_API_KEY}`);
+        return response.data[0];
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error("Axios error fetching company profile:", error.message);
+            return error.message;
+        }
+
+        console.error("Unexpected error fetching company profile:", error);
+        return "An unexpected error occurred while fetching the company profile.";
+    }
+}
+
+export const getCompanyQuote = async (ticker: string) => {
+    try {
+        const response = await axios.get(`https://financialmodelingprep.com/stable/quote?symbol=${ticker}&apikey=${process.env.NEXT_PUBLIC_FMP_API_KEY}`);
+        return response.data[0];
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error("Axios error fetching company quote:", error.message);
+            return error.message;
+        }
+
+        console.error("Unexpected error fetching company quote:", error);
+        return "An unexpected error occurred while fetching the company quote.";
     }
 }
--- finshark-frontend/src/Views/CompanyPage/CompanyPage.tsx
+++ finshark-frontend/src/Views/CompanyPage/CompanyPage.tsx
@@ -1,11 +1,45 @@
 "use client";
 
+import { CompanyProfile } from "@/company";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getCompanyProfile } from "../../../api";
 
 const CompanyPage = () => {
   const { ticker } = useParams();
-  return <div>Company: {ticker?.toUpperCase()}</div>;
+  const [company, setCompany] = useState<CompanyProfile>();
+  const [serverError, setServerError] = useState<string>("");
+
+  useEffect(() => {
+    const fetchCompanyProfile = async () => {
+      if (!ticker) return;
+      const response = await getCompanyProfile(ticker);
+      if (typeof response === "string") {
+        setServerError(response);
+        setCompany(undefined);
+      } else {
+        setServerError("");
+        setCompany(response);
+      }
+    };
+    void fetchCompanyProfile();
+  }, [ticker]);
+      
+  return (
+    <div>
+      {serverError && <p>{serverError}</p>}
+      {company ? (
+        <div>
+          <h1>{company.companyName} ({company.symbol})</h1>
+          <p>{company.description}</p>
+          <p>Price: ${company.price}</p>
+        </div>
+      ) : (
+        <p>Company not found</p>
+      )}  
+      Company: {ticker?.toUpperCase()}
+    </div>
+  )
 };
 
 export default CompanyPage;
-
